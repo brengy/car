6c77d76606e9e46227d7bdb729035d8d672870e9
carQ <59324191+brengy@users.noreply.github.com>
2024-07-03 05:30:52 +0300

    Add files via upload

---
+++ about.js
@@ -0,0 +1,31 @@
+const buttonSound = document.getElementById('buttonSound');
+
+function playSound() {
+    buttonSound.currentTime = 0; // Rewind to the start
+    buttonSound.play();
+}
+
+document.getElementById('mainButton').addEventListener('click', function() {
+    playSound();
+    window.location.href = 'main.html';
+});
+
+document.getElementById('monitorButton').addEventListener('click', function() {
+    playSound();
+    window.location.href = 'monitor.html';
+});
+
+document.getElementById('gpsButton').addEventListener('click', function() {
+    playSound();
+    window.location.href = 'gps.html';
+});
+
+document.getElementById('networkButton').addEventListener('click', function() {
+    playSound();
+    window.location.href = 'network.html';
+});
+
+document.getElementById('indButton').addEventListener('click', function() {
+    playSound();
+    window.location.href = 'index.html';
+});
